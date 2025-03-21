d6cf6b76ff15bcb461f2fd0056a59547824e367b
SolanetJose <jopytr123@duck.com>
2025-03-21 11:02:06 -0300

    Update exercise_math.py
    TP1 Jose Solanet Ingenieria Industrial 2025

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+    c=(a + b)
+    d=(a - b)
+    e=(a * b)
+    f=(c / 2)
+    g=(a // b)
+    h=(a % b)
+    i=(a / b)
+    print(c)
+    print(d)
+    print(e)
+    print(f)
+    print(g)
+    print(h)
+    print(i)
